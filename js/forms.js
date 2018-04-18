cc29082d0d2f4c98fd65e0fc368c775b2ba33e02
CJ <chasanid@gmail.com>
2018-04-18 00:12:45 -0400

    test mailer

--- js/forms.js
+++ js/forms.js
@@ -113,6 +113,9 @@
 								},
 								success: function(){
 									_.showFu()
+								},
+								error: function(err) {
+									console.log(err)
 								}
 							})			
 					},
@@ -156,13 +159,13 @@
 						_.controlsFu()
 														
 						_.form
-							// .bind('submit',function(){
-							// 	if(_.validate)
-							// 		_.submitFu()
-							// 	else
-							// 		_.form[0].submit()
-							// 	return false
-							// })
+							.bind('submit',function(){
+								if(_.validate)
+									_.submitFu()
+								else
+									_.form[0].submit()
+								return false
+							})
 							.bind('reset',function(){
 								_.labels.removeClass(_.invalidCl)									
 								_.labels.each(function(){
