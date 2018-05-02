b5d584e48cba3c8a05f0aff65d878ca6136ff582
Warren <uberpwn370@gmail.com>
2018-05-02 15:30:03 -0400

    Warren's attempt at PHP

--- mailer_old.php
+++ mailer_old.php
@@ -1,5 +1,5 @@
 <?php
- use PHPMailer\PHPMailer\PHPMailer
+
  require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
 
 $name = $_POST['name'];
@@ -8,9 +8,11 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 $departmentRaw = $_POST['department'];
 $department = 'Unspecified';
-$sendTo = ['info@suresteprisk.com','wlucas@suresteprisk.com','mgibbs@suresteprisk.com'];
+$sendTo = ['info@suresteprisk.com'];
 
-/*
+$mikeEmail = 'mgibbs@suresteprisk.com';
+$warrieEmail = 'wlucas@suresteprisk.com';
+
 switch ($departmentRaw) {
 	case 'governance':
 		echo 'governance';
@@ -45,22 +47,22 @@ switch ($departmentRaw) {
 	default:
 		echo $departmentRaw;
 }
-*/
 
-$mail = new PHPMailer(TRUE);
+$mail = new PHPMailer;
 
 $mail->isSMTP();
-$mail->Host = 'smtp.office365.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'info@suresteprisk.com';
-$mail->Password = 'Superman123!';
-$mail->SMTPSecure = 'tls';
-$mail->Port = 25;
-
-$mail->setFrom($email, $name);
-$mail->addAddress($sendTo)
 $mail->isHTML(true);
-
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Password = 'plz_dont_steal';
+$mail->Username = 'personal.portfolio.cj@gmail.com';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+ 
+$mail->setFrom($email, $name);
+foreach ($sendTo as $emailAddress) {
+    $mail->addAddress($emailAddress);
+}
 $mail->Subject = "Website Contact From {$name}";
 $mail->Body    = "<strong>Who:</strong> {$name} <br><br> <strong>Phone:</strong> {$phone} <br><br> <strong>Department:</strong> {$department} <br><br> <strong>Message:</strong> {$message}";
  
@@ -70,4 +72,5 @@ if(!$mail->send()) {
 } else {
     echo 'Message has been sent';
 }
+
  ?>
