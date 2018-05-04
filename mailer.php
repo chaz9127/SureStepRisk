6476fca9b564b50dec33fec6b8d12ba7292ebeec
CJ <chasanid@gmail.com>
2018-05-04 08:56:02 -0400

    final mailer push

--- mailer.php
+++ mailer.php
@@ -77,21 +77,69 @@
 
 
 
-<?php
-// using SendGrid's PHP Library
-// https://github.com/sendgrid/sendgrid-php
-// If you are using Composer (recommended)
+
 require 'vendor/autoload.php';
-// If you are not using Composer
-// require("path/to/sendgrid-php/sendgrid-php.php");
-$from = new SendGrid\Email("Example User", "test@example.com");
-$subject = "Sending with SendGrid is Fun";
-$to = new SendGrid\Email("Example User", "test@example.com");
-$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-$response = $sg->client->mail()->send()->post($mail);
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$departmentRaw = $_POST['department'];
+$department = 'Unspecified';
+$sendTo = ['info@suresteprisk.com'];
+
+$mikeEmail = 'mgibbs@suresteprisk.com';
+$warrieEmail = 'wlucas@suresteprisk.com';
+
+switch ($departmentRaw) {
+	case 'governance':
+		echo 'governance';
+		$department = 'Governance';
+		array_push($sendTo, $mikeEmail, $warrieEmail);
+		break;
+	case 'risk':
+		echo 'risk';
+		$department = 'Risk and Compliance';
+		array_push($sendTo, $mikeEmail, $warrieEmail);
+		break;
+	case 'managed-services':
+		echo 'managed-services';
+		$department = 'Managed Services';
+		array_push($sendTo, $mikeEmail);
+		break;
+	case 'general':
+		echo 'general';
+		$department = 'General';
+		array_push($sendTo, $mikeEmail, $warrieEmail);
+		break;
+	case 'hosting':
+		echo 'hosting';
+		$department = 'Hosting';
+		array_push($sendTo, $mikeEmail, $warrieEmail);
+		break;
+	case 'data':
+		echo 'data';
+		$department = 'Data';
+		array_push($sendTo, $warrieEmail);
+		break;
+	default:
+		echo $departmentRaw;
+}
+
+$from = new SendGrid\Email($name, "do_not_reply@sendgrid.com");
+$subject = "Website contact from: {$name}";
+$apiKey = 'SG.SmzzGMoLRFGb-mlOsZQoqA.7Jx_VGSR_-9W-bL1uV-gAUZFxYMGnn-wIRWA9quBA8s';
+$contentString = "<strong>Who:</strong> {$name} <br><br> <strong>Email:</strong> {$email} <br><br> <strong>Phone:</strong> {$phone} <br><br> <strong>Department:</strong> {$department} <br><br> <strong>Message:</strong> {$message}";
+$content = new SendGrid\Content("text/html", $contentString);
+
+
+foreach ($sendTo as $emailAddress) {
+    $to = new SendGrid\Email("SureStep", $emailAddress);
+	$mail = new SendGrid\Mail($from, $subject, $to, $content);
+	$sg = new \SendGrid($apiKey);
+	$response = $sg->client->mail()->send()->post($mail);
+}
+
+
 echo $response->statusCode();
 print_r($response->headers());
 echo $response->body();
