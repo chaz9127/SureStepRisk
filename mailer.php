840d302c7ce4ef471efc9f43f7b09c2fae593913
CJ <chasanid@gmail.com>
2018-05-04 08:58:46 -0400

    mailer cleanup

--- mailer.php
+++ mailer.php
@@ -1,83 +1,4 @@
 <?php
-
-//  require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
-
-// $name = $_POST['name'];
-// $email = $_POST['email'];
-// $phone = $_POST['phone'];
-// $message = $_POST['message'];
-// $departmentRaw = $_POST['department'];
-// $department = 'Unspecified';
-// $sendTo = ['info@suresteprisk.com'];
-
-// $mikeEmail = 'mgibbs@suresteprisk.com';
-// $mikeEmail = '';
-// $warrieEmail = 'wlucas@suresteprisk.com';
-// $warrieEmail = '';
-
-// switch ($departmentRaw) {
-// 	case 'governance':
-// 		echo 'governance';
-// 		$department = 'Governance';
-// 		// array_push($sendTo, $mikeEmail, $warrieEmail);
-
-// 		break;
-// 	case 'risk':
-// 		echo 'risk';
-// 		$department = 'Risk and Compliance';
-// 		array_push($sendTo, $mikeEmail, $warrieEmail);
-// 		break;
-// 	case 'managed-services':
-// 		echo 'managed-services';
-// 		$department = 'Managed Services';
-// 		array_push($sendTo, $mikeEmail);
-// 		break;
-// 	case 'general':
-// 		echo 'general';
-// 		$department = 'General';
-// 		array_push($sendTo, $mikeEmail, $warrieEmail);
-// 		break;
-// 	case 'hosting':
-// 		echo 'hosting';
-// 		$department = 'Hosting';
-// 		array_push($sendTo, $mikeEmail, $warrieEmail);
-// 		break;
-// 	case 'data':
-// 		echo 'data';
-// 		$department = 'Data';
-// 		array_push($sendTo, $warrieEmail);
-// 		break;
-// 	default:
-// 		echo $departmentRaw;
-// }
-
-// $mail = new PHPMailer;
-
-// $mail->isSMTP();
-// $mail->isHTML(true);
-// $mail->Host = 'relay-hosting.secureserver.net';
-// $mail->SMTPAuth = false;
-// $mail->SMTPSecure = false;
-// $mail->Port = 25;
-// $mail->SMTPDebug  = 3;
-// $mail->Debugoutput = function($str, $level) {echo "debug level $level; message: $str";};
-// $mail->setFrom('info@suresteprisk.com', $name);
-// foreach ($sendTo as $emailAddress) {
-//     $mail->addAddress($emailAddress);
-// }
-// $mail->Subject = "Website Contact From {$name}";
-// $mail->Body    = "<strong>Who:</strong> {$name} <br><br> <strong>Phone:</strong> {$phone} <br><br> <strong>Department:</strong> {$department} <br><br> <strong>Message:</strong> {$message}";
- 
-// if(!$mail->send()) {
-//     echo 'Message could not be sent.';
-//     echo 'Mailer Error: ' . $mail->ErrorInfo;
-// } else {
-//     echo 'Message has been sent';
-// }
-
-
-
-
 require 'vendor/autoload.php';
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -92,37 +13,31 @@ $warrieEmail = 'wlucas@suresteprisk.com';
 
 switch ($departmentRaw) {
 	case 'governance':
-		echo 'governance';
 		$department = 'Governance';
 		array_push($sendTo, $mikeEmail, $warrieEmail);
 		break;
 	case 'risk':
-		echo 'risk';
 		$department = 'Risk and Compliance';
 		array_push($sendTo, $mikeEmail, $warrieEmail);
 		break;
 	case 'managed-services':
-		echo 'managed-services';
 		$department = 'Managed Services';
 		array_push($sendTo, $mikeEmail);
 		break;
 	case 'general':
-		echo 'general';
 		$department = 'General';
 		array_push($sendTo, $mikeEmail, $warrieEmail);
 		break;
 	case 'hosting':
-		echo 'hosting';
 		$department = 'Hosting';
 		array_push($sendTo, $mikeEmail, $warrieEmail);
 		break;
 	case 'data':
-		echo 'data';
 		$department = 'Data';
 		array_push($sendTo, $warrieEmail);
 		break;
 	default:
-		echo $departmentRaw;
+		
 }
 
 $from = new SendGrid\Email($name, "do_not_reply@sendgrid.com");
@@ -140,7 +55,7 @@ foreach ($sendTo as $emailAddress) {
 }
 
 
-echo $response->statusCode();
-print_r($response->headers());
-echo $response->body();
+// echo $response->statusCode();
+// print_r($response->headers());
+// echo $response->body();
  ?>
