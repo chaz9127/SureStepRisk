122e4fe6aeaa2665f94a7d0683a376ab6dbce566
CJ <chasanid@gmail.com>
2018-05-04 02:12:58 -0400

    installing composer

--- mailer.php
+++ mailer.php
@@ -1,80 +1,98 @@
 <?php
 
- require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+//  require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$departmentRaw = $_POST['department'];
-$department = 'Unspecified';
-$sendTo = ['info@suresteprisk.com'];
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $phone = $_POST['phone'];
+// $message = $_POST['message'];
+// $departmentRaw = $_POST['department'];
+// $department = 'Unspecified';
+// $sendTo = ['info@suresteprisk.com'];
 
-$mikeEmail = 'mgibbs@suresteprisk.com';
-$mikeEmail = '';
-$warrieEmail = 'wlucas@suresteprisk.com';
-$warrieEmail = '';
+// $mikeEmail = 'mgibbs@suresteprisk.com';
+// $mikeEmail = '';
+// $warrieEmail = 'wlucas@suresteprisk.com';
+// $warrieEmail = '';
 
-switch ($departmentRaw) {
-	case 'governance':
-		echo 'governance';
-		$department = 'Governance';
-		// array_push($sendTo, $mikeEmail, $warrieEmail);
+// switch ($departmentRaw) {
+// 	case 'governance':
+// 		echo 'governance';
+// 		$department = 'Governance';
+// 		// array_push($sendTo, $mikeEmail, $warrieEmail);
 
-		break;
-	case 'risk':
-		echo 'risk';
-		$department = 'Risk and Compliance';
-		array_push($sendTo, $mikeEmail, $warrieEmail);
-		break;
-	case 'managed-services':
-		echo 'managed-services';
-		$department = 'Managed Services';
-		array_push($sendTo, $mikeEmail);
-		break;
-	case 'general':
-		echo 'general';
-		$department = 'General';
-		array_push($sendTo, $mikeEmail, $warrieEmail);
-		break;
-	case 'hosting':
-		echo 'hosting';
-		$department = 'Hosting';
-		array_push($sendTo, $mikeEmail, $warrieEmail);
-		break;
-	case 'data':
-		echo 'data';
-		$department = 'Data';
-		array_push($sendTo, $warrieEmail);
-		break;
-	default:
-		echo $departmentRaw;
-}
+// 		break;
+// 	case 'risk':
+// 		echo 'risk';
+// 		$department = 'Risk and Compliance';
+// 		array_push($sendTo, $mikeEmail, $warrieEmail);
+// 		break;
+// 	case 'managed-services':
+// 		echo 'managed-services';
+// 		$department = 'Managed Services';
+// 		array_push($sendTo, $mikeEmail);
+// 		break;
+// 	case 'general':
+// 		echo 'general';
+// 		$department = 'General';
+// 		array_push($sendTo, $mikeEmail, $warrieEmail);
+// 		break;
+// 	case 'hosting':
+// 		echo 'hosting';
+// 		$department = 'Hosting';
+// 		array_push($sendTo, $mikeEmail, $warrieEmail);
+// 		break;
+// 	case 'data':
+// 		echo 'data';
+// 		$department = 'Data';
+// 		array_push($sendTo, $warrieEmail);
+// 		break;
+// 	default:
+// 		echo $departmentRaw;
+// }
 
-$mail = new PHPMailer;
+// $mail = new PHPMailer;
 
-$mail->isSMTP();
-$mail->isHTML(true);
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Password = 'plz_dont_steal';
-$mail->Username = 'personal.portfolio.cj@gmail.com';
-$mail->SMTPSecure = 'tls';
-$mail->Port = 587;
- $mail->SMTPDebug  = 3;
-$mail->Debugoutput = function($str, $level) {echo "debug level $level; message: $str";};
-$mail->setFrom('info@suresteprisk.com', $name);
-foreach ($sendTo as $emailAddress) {
-    $mail->addAddress($emailAddress);
-}
-$mail->Subject = "Website Contact From {$name}";
-$mail->Body    = "<strong>Who:</strong> {$name} <br><br> <strong>Phone:</strong> {$phone} <br><br> <strong>Department:</strong> {$department} <br><br> <strong>Message:</strong> {$message}";
+// $mail->isSMTP();
+// $mail->isHTML(true);
+// $mail->Host = 'relay-hosting.secureserver.net';
+// $mail->SMTPAuth = false;
+// $mail->SMTPSecure = false;
+// $mail->Port = 25;
+// $mail->SMTPDebug  = 3;
+// $mail->Debugoutput = function($str, $level) {echo "debug level $level; message: $str";};
+// $mail->setFrom('info@suresteprisk.com', $name);
+// foreach ($sendTo as $emailAddress) {
+//     $mail->addAddress($emailAddress);
+// }
+// $mail->Subject = "Website Contact From {$name}";
+// $mail->Body    = "<strong>Who:</strong> {$name} <br><br> <strong>Phone:</strong> {$phone} <br><br> <strong>Department:</strong> {$department} <br><br> <strong>Message:</strong> {$message}";
  
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
+// if(!$mail->send()) {
+//     echo 'Message could not be sent.';
+//     echo 'Mailer Error: ' . $mail->ErrorInfo;
+// } else {
+//     echo 'Message has been sent';
+// }
 
+
+
+<?php
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
+// If you are using Composer (recommended)
+require 'vendor/autoload.php';
+// If you are not using Composer
+// require("path/to/sendgrid-php/sendgrid-php.php");
+$from = new SendGrid\Email("Example User", "test@example.com");
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Example User", "test@example.com");
+$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+print_r($response->headers());
+echo $response->body();
  ?>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,21 +6,32 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitc906c090bf39a72ee32e7dc088138a87
 {
-    public static $classMap = array (
-        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
-        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
-        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
-        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
-        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
-        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
-        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
-        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+    public static $files = array (
+        '3f8bdd3b35094c73a26f0106e3c0f8b2' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/SendGrid.php',
+        '9dda55337a76a24e949fbcc5d905a2c7' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/helpers/mail/Mail.php',
+        '5d7e2090c9a3b69b2ff2fceddeadca94' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/helpers/contacts/Recipients.php',
+        '04a9a7b26fb79fbd712347cc9bc9d9be' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/helpers/stats/Stats.php',
+    );
+
+    public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'SendGrid\\' => 9,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'SendGrid\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sendgrid/php-http-client/lib',
+        ),
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->classMap = ComposerStaticInitc906c090bf39a72ee32e7dc088138a87::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitc906c090bf39a72ee32e7dc088138a87::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitc906c090bf39a72ee32e7dc088138a87::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
